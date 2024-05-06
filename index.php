7bdb1998748ec4d24cb87d808815c0959d95093f
alevizper <alevizper@alu.edu.gva.es>
2024-05-06 17:02:58 +0200

    V 0.2: Añadido el navbar

--- index.php
+++ index.php
@@ -4,8 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Magno Games</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    
 </head>
 <body>
+<header data-bs-theme="dark">
+  <div class="navbar navbar-dark navbar-expand-lg bg-dark shadow-sm">
+    <div class="container">
+      <a href="#" class="navbar-brand">
+        <strong>Magno Games</strong>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarHeader">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li clas="nav-item">
+            <a href="#" class="nav-link active">Todos los juegos</a>
+          </li>
+          <li>  
+          <a href="#" class="nav-link">Contacto</a>
+          </li>
+        </ul>
+        <a href="carrito.php" class="btn btn-primary">Carrito</a>
+      </div>
+    </div>
+  </div>
+</header>
+
+<main>
+  <div class="container">
     
+  </div>
+</main>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+crossorigin="anonymous"></script>
 </body>
 </html>
